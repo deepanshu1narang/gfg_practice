b10ed29c3896469c1751b294e5d83b6320443737
deepanshu1narang <deepanshunarang19@gmail.com>
2025-05-20 00:22:18 +0530

    stack ques

--- chap-11_Stack_Queue_Dequeue/stack_ques.js
+++ chap-11_Stack_Queue_Dequeue/stack_ques.js
@@ -26,7 +26,7 @@ let nums = [6, 8, 0, 1, 3];
 nums = [1, 3, 2, 4];
 nums = [41, 88, 58, 69, 93, 42, 44, 25, 12, 47, 41, 88, 58, 69, 93, 42, 44, 25, 12, 47];
 let nge1 = nextGreaterEelment1(nums);
-console.log(nge1);
+// console.log(nge1);
 
 function nextGreaterEelment1WithoutSpace(nums) {
     let result = new Array(nums.length).fill(-1);
@@ -43,6 +43,27 @@ function nextGreaterEelment1WithoutSpace(nums) {
 
 // next greater element 1 -with 2 arrays 
 // https://leetcode.com/problems/next-greater-element-i/
+// function nextGreaterElement2(nums: number[]): number[]
+function nextGreaterElement2(nums) {
+    const result = new Array(nums.length).fill(-1);
+    const stack = [];
+
+    // bcoz the greater one can occur before the occurrence of the element itself.. so iterating it to 2*n
+    for (let i = 0; i < 2 * nums.length; i++) {
+        // since we are iterating upto 2 * n so we need to use modulo so that we can stay inside array
+        const idx = i % nums.length;
+        while (stack.length > 0 && nums[idx] > nums[stack[stack.length - 1]]) {
+            // peek as in top element of the stack and the one gonna be removed
+            const peek = stack.pop();
+            // if that is one then only we'll take the chance to update it else it already got its greater element so no ned to bother it 
+            if (result[peek] === -1) {
+                result[peek] = nums[idx];
+            }
+        }
+        stack.push(idx);
+    }
+    return result;
+}
 
 // function nextGreaterElementWithTwoArrays(nums1: number[], nums2: number[]): number[] 
 function nextGreaterElementWithTwoArrays(nums1, nums2) {
@@ -80,10 +101,35 @@ function nextSmallerElement(nums) {
 
 nums = [4, 8, 2, 1, 6, 10, 5];
 let nse1 = nextSmallerElement(nums);
-console.log(nse1);
+// console.log(nse1);
 
 // nge 2 --- circular array
 // function nextGreaterElement2(nums: number[]): number[]
 function nextGreaterElement2(nums) {
     return []
 }
+
+function previousSmallerElement(nums) { }
+
+// smallest-missing-integer-greater-than-sequential-prefix-sum
+// https://leetcode.com/problems/smallest-missing-integer-greater-than-sequential-prefix-sum/
+
+function smallestMissingNumber(nums) {
+    const numsSet = new Set(nums);
+    
+    let prefixSum = nums[0];
+    let i = 1;
+    while(i < nums.length && nums[i] === nums[i - 1] + 1){
+        prefixSum += nums[i];
+        i++;
+    }
+    while(numsSet.has(prefixSum)){
+        prefixSum++;
+    }
+    return prefixSum;
+};
+
+// console.log(smallestMissingNumber([12, 13, 14, 1, 3, 4, 5]));
+// console.log(smallestMissingNumber([3,4,5,1,12,14,13]));
+// console.log(smallestMissingNumber([1,2,3,2,5]));
+console.log(smallestMissingNumber([14,9,6,9,7,9,10,4,9,9,4,4]));
