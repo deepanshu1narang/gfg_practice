ff1b4412bc456e5861d40cd23f582f5c35ffdf4f
deepanshu1narang <99411892+deepanshu1narang@users.noreply.github.com>
2025-11-21 00:48:36 +0530

    Update print_all_subsequences.js

--- chap-3_recursion/print_all_subsequences.js
+++ chap-3_recursion/print_all_subsequences.js
@@ -1,10 +1,10 @@
-Print all subsequences of an array
+// 1. Print all subsequences of an array
 
 const arr2 = [3, 1, 2];
 const n = arr2.length;
 function getSubsequences(idx = 0, list = []){
     if(idx >= n){
-        console.log(list, "result");
+        console.log(list);
         return;
     }
     
@@ -15,3 +15,31 @@ function getSubsequences(idx = 0, list = []){
 }
 
 getSubsequences(0, []);
+
+// 2. Print all the subsequences whose sum is K (a given number)
+function getCountAndSubsequences(arr, k){
+    let value = {x: 0};
+
+    return printSubsequences([1,2,1], 2, value, 0, [], 0);
+}
+
+function printSubsequences(arr, k, value, idx = 0, list = [], sum = 0){
+    if(idx === arr.length){
+        if(sum === k){
+            console.log(list);
+            value.x++;
+        }
+        return;
+    }
+    
+    list.push(arr[idx]);
+    sum += arr[idx];
+    printSubsequences(arr, k, value, idx + 1, list, sum);
+    list.pop();
+    sum -= arr[idx];
+    
+    printSubsequences(arr, k, value, idx + 1, list, sum);
+    return value.x;
+}
+
+console.log(getCountAndSubsequences([1,2,1], 2));
